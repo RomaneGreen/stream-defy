cc9bec84d20489086aa69492ff1be3ee0fad50ad
Romane Green <Romane71193@gmail.com>
2018-12-06 10:48:11 -0500

    components

---
+++ src/components/appHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import '../App.css';
+
+ class AppHeader extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className = "headerz">Musice</h1>
+      
+      </div>
+    )
+  }
+}
+
+export default AppHeader;
