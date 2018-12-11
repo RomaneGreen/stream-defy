726107e08b7953aa401718775f26e40148365649
Romane Green <Romane71193@gmail.com>
2018-12-10 20:17:58 -0500

    add css modify testing

---
+++ src/setupTests.js
@@ -0,0 +1,8 @@
+// import { configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// configure({ adapter: new Adapter() });
+
+// const enzyme = require("enzyme");
+// const adapter = require("enzyme-adapter-react-16");
+
+// enzyme.configure({ adapter: new Adapter() });
