941dfc5e7991c82cf921de56d41223c421310e63
Romane Green <Romane71193@gmail.com>
2018-12-13 03:00:21 -0500

    import test modules

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import * as actions from './entities/playlists/actions'
+import initializePlaylist from './entities/playlists/reducers'
+import {
+ON_PAGE_SELECT,
+ON_PLAYLIST_INIT,
+LOAD_PLAYLIST,
+SAVE_SONG_DATA,
+ON_SEARCH_PLAYLIST_INIT,
+} from './entities/playlists/actions';
