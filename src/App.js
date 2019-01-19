06d75a9515d2b077e214f95086221d792d37a8dd
Romane G <romane71193@gmail.com>
2019-01-19 13:26:38 -0500

    linted

--- src/App.js
+++ src/App.js
@@ -15,142 +15,145 @@ import { onPlaylistInit, onPageSelect } from './entities';
 // Containers
 import AppContents from './components/app-contents';
 
-
 // Css
 import './App.css';
 
 // This Container
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      user: null,
-    }
+      user: null
+    };
 
-    let genresArray = _.map(this.props.playlistList, item => (item.genreSlug));
+    const genresArray = _.map(this.props.playlistList, item => item.genreSlug);
     genresArray.push('browse');
     this.genres = genresArray;
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
     this.cookies = new Cookies();
 
-    if (this.state.user){
-      base.bindToState(`${this.props.user.uid}Playlists`, {
+    if (this.state.user) {
+      base.bindToState(`${ this.props.user.uid }Playlists`, {
         context: this,
         state: 'items'
       });
     }
-
   }
 
-  componentWillMount(){
-    _.map(this.props.playlistList, (item) => {
-      return(
-        _.map(item.playlists, (playlist) => {
-          return(
-            this.props.onPlaylistInit(playlist.playlistId, item.genreSlug)
-          );
-        })
-      );
-    })
+  componentWillMount() {
+    _.map(this.props.playlistList, item => {
+      return _.map(item.playlists, playlist => {
+        return this.props.onPlaylistInit(playlist.playlistId, item.genreSlug);
+      });
+    });
 
-    if (this.cookies.get('cookie')){
-      let cookie = this.cookies.get('cookie');
+    if (this.cookies.get('cookie')) {
+      const cookie = this.cookies.get('cookie');
       this.setState({
         user: cookie.user
-      })
+      });
     }
   }
 
   login() {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(
+        function(result) {
+          var token = result.credential.accessToken;
+          this.cookies.set(
+            'cookie',
+            { token: token, user: result.user },
+            { path: '/', expires: expiry() }
+          );
 
-      var token = result.credential.accessToken;
-      this.cookies.set('cookie', {token: token, user: result.user}, { path: '/', expires: expiry() });
+          this.setState({
+            user: result.user
+          });
 
-      this.setState({
-        user: result.user
-      });
-
-      // Check if user exists in firebase
-      base.fetch(result.user.uid, {
-        context: this,
-        then(data){
-          if (!data.userInfo){
-            base.post(`${result.user.uid}`, {
-              data: {
-                'userInfo':
-                  {
-                  name: result.user.displayName,
-                  email: result.user.email,
-                  uid: result.user.uid
-                },
-                'savedSongs': []
+          // Check if user exists in firebase
+          base.fetch(result.user.uid, {
+            context: this,
+            then(data) {
+              if (!data.userInfo) {
+                base.post(`${ result.user.uid }`, {
+                  data: {
+                    userInfo: {
+                      name: result.user.displayName,
+                      email: result.user.email,
+                      uid: result.user.uid
+                    },
+                    savedSongs: []
+                  }
+                });
               }
-            })
-          }
-        }
-      });
-
-    }.bind(this));
+            }
+          });
+        }.bind(this)
+      );
   }
 
   logout() {
-    firebase.auth().signOut().then(function() {
-      this.setState({user: null});
-      this.cookies.remove('cookie');
-    }.bind(this));
+    firebase
+      .auth()
+      .signOut()
+      .then(
+        function() {
+          this.setState({ user: null });
+          this.cookies.remove('cookie');
+        }.bind(this)
+      );
   }
 
   render() {
-
     return (
-      
-      <div className="App">
-         
-      
-        <AppHeader />
-        
-        <AppContents
-          
-          showPage={this.props.showPage}
-          playlistList={this.props.playlistList}
-          user={this.state.user}
-          onPageSelect={this.props.onPageSelect}
-          genres={this.genres}
-          key={`${this.state.user}-app`}
+        <div className="App">
+            <AppHeader />
+
+            <AppContents
+          showPage={ this.props.showPage }
+          playlistList={ this.props.playlistList }
+          user={ this.state.user }
+          onPageSelect={ this.props.onPageSelect }
+          genres={ this.genres }
+          key={ `${ this.state.user }-app` }
         />
 
-        { this.state.user ? (
+            {this.state.user ? (
+                <div className="user-info">
+                    <img src={ this.state.user.photoURL } alt="" />
+                    <span>{this.state.user.displayName}</span>
+                    <button className="logout" onClick={ this.logout }>
+              Logout
+                    </button>
+                </div>
+        ) : (
             <div className="user-info">
-            <img src={this.state.user.photoURL} alt=""/>
-            <span>{this.state.user.displayName}</span>
-            <button className="logout" onClick={this.logout}>Logout</button>
+                <button className="login" onClick={ this.login }>
+              Login
+                </button>
             </div>
-          ) : (
-            <div className="user-info">
-            <button className="login" onClick={this.login}>Login</button>
-            </div>
-          )
-        }
-
-      </div>
+        )}
+        </div>
     );
   }
-
 }
 
-function mapStateToProps(state){
-  return{
+function mapStateToProps(state) {
+  return {
     playlistList: state.playlistList,
     showPage: state.playlists.showPage,
-    cachedPlaylists: state.playlists.cachedPlaylists,
+    cachedPlaylists: state.playlists.cachedPlaylists
   };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
   return bindActionCreators({ onPlaylistInit, onPageSelect }, dispatch);
 }
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
